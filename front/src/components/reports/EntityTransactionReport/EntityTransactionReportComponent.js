cf00b3dd9a109c3426ea2b2987b6139a8915a98b
MokhtarMahmoud <mokhtarmm17@gmail.com>
2024-12-10 09:06:04 +0200

    mokhtar 9/12

--- front/src/components/reports/EntityTransactionReport/EntityTransactionReportComponent.js
+++ front/src/components/reports/EntityTransactionReport/EntityTransactionReportComponent.js
@@ -14,14 +14,26 @@ export default function EntityTransactionReportComponent() {
   const [tableData, setTableData] = useState([]); // Data to be displayed in the table
   const [users, setUsers] = useState([])
 
+  const [report, setReport] = useState([]); // Track first select value
 
-  console.log("tableData",tableData)
-  useEffect(() => {
-    if (selectedValue === null) {
-      setSelectedValue(null);
-      setSecondSelectValue(null);
-    }
-  }, [selectedValue]);
+  const getReport = async () => {
+    if(!selectedValue || !secondSelectValue){
+      return  toast.error('يجب اختيار الجهة');
+      }
+
+    const data = {
+      // invoiceCode: selectedValue ,
+      supplierID: secondSelectValue,
+      startDate: startDate,
+      endDate: endDate,
+    };
+    const reports = await window?.electron?.searchForReport(data)
+    console.log('reports', reports);
+    
+    setReport(reports);
+  }
+
+  console.log("report",report?.categoryObject)
 
 
   useEffect(() => {
@@ -77,67 +89,25 @@ export default function EntityTransactionReportComponent() {
 
 
 
-  const staticData = [
-    {
-      code: 'A001',
-      number: "1",
-      type: "صرف",
-      name: 'جهة صرف 1',
-      invoiceDate: '20-11-2024',
-      registrationDate: "20-11-2024"
-    },
-    {
-      code: 'A002',
-      number: "2",
-      type: "تحصيل",
-      name: 'جهة تحصيل 1',
-      invoiceDate: '21-11-2024',
-      registrationDate: "21-11-2024"
-    },
-    {
-      code: 'A003',
-      number: "3",
-      type: "صرف",
-      name: 'جهة صرف 2',
-      invoiceDate: '22-11-2024',
-      registrationDate: "22-11-2024"
-    },
-    {
-      code: 'A004',
-      number: "4",
-      type: "تحصيل",
-      name: 'جهة تحصيل 2',
-      invoiceDate: '23-11-2024',
-      registrationDate: "23-11-2024"
-    },
-    {
-      code: 'A005',
-      number: "5",
-      type: "صرف",
-      name: 'جهة صرف 3',
-      invoiceDate: '24-11-2024',
-      registrationDate: "24-11-2024"
-    },
-    {
-      code: 'A006',
-      number: "6",
-      type: "تحصيل",
-      name: 'جهة تحصيل 3',
-      invoiceDate: '25-11-2024',
-      registrationDate: "25-11-2024"
-    }
-  ];
-
   const numberColumnHeader = selectedValue === '2' ? "رقم اذن الصرف" : "رقم الفاتوره";
   const nameColumnHeader = selectedValue === '1' ? "اسم جهة التوريد" : selectedValue === '2' ? "اسم جهة الصرف" : "اسم جهة التحويل";
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+  
+    return `${day}-${month}-${year}`;
+  };
 
   const columns = [
     {
       name: 'كود الفاتوره',
-      minwidth: '180px',
+      width: '180px',
       sortable: true,
       cell: row => {
-        let codeStr = row?.code?.length > 10 ? row.code.substring(0, 10) + '...' : row.code;
+        let codeStr = row.invoiceCode ;
         return (
           <div style={{ textAlign: 'center', whiteSpace: 'normal', wordWrap: 'break-word', width: '100%' }}>
             {codeStr}
@@ -147,10 +117,10 @@ export default function EntityTransactionReportComponent() {
     },
     {
       name: numberColumnHeader,
-      minwidth: '180px',
+      width: '180px',
       sortable: true,
       cell: row => {
-        let numberStr = row?.number?.length > 10 ? row.numberStr.substring(0, 10) + '...' : row?.number;
+        let numberStr =row?.serialNumber;
         return (
           <div style={{ textAlign: 'center', whiteSpace: 'normal', wordWrap: 'break-word', width: '100%' }}>
             {numberStr}
@@ -160,26 +130,26 @@ export default function EntityTransactionReportComponent() {
     },
     {
       name: 'نوع الفاتورة',
-      minwidth: '180px',
+      width: '180px',
       selector: row => row.type,
       sortable: true,
     },
     {
       name: nameColumnHeader,
-      minwidth: '200px',
+      width: '200px',
       selector: row => row.name,
       sortable: true,
     },
     {
       name: 'تاريخ الفاتورة',
-      minwidth: '180px',
-      selector: row => row.invoiceDate,
+      width: '180px',
+    selector: row => formatDate(row.supplyDate),
       sortable: true,
     },
     {
       name: 'تاريخ التسجيل',
-      minwidth: '180px',
-      selector: row => row.registrationDate,
+      width: '180px',
+      selector: row => formatDate(row.registerDate),
       sortable: true,
     },
     {
@@ -211,72 +181,7 @@ export default function EntityTransactionReportComponent() {
     },
   };
 
-  const search = () => {
-    
-    if(!selectedValue || !secondSelectValue){
-      return  toast.error('يجب اختيار الجهة');
-      }
-    let filteredData = staticData;
-  
-    // Helper function to convert DD-MM-YYYY to Date object
-    const formatDateToObject = (dateString) => {
-      let formattedDate;
-    
-      // Check if the dateString is in YYYY-MM-DD format (standard format supported by JavaScript Date object)
-      const regex = /^\d{4}-\d{2}-\d{2}$/;
-      
-      if (regex.test(dateString)) {
-        // Directly parse YYYY-MM-DD format
-        formattedDate = new Date(dateString);
-      } else {
-        // Split by "-" for the DD-MM-YYYY format
-        const parts = dateString.split("-");
-        
-        if (parts.length === 3) {
-          // Handle DD-MM-YYYY format by reordering to YYYY-MM-DD for compatibility
-          const [day, month, year] = parts;
-          formattedDate = new Date(`${year}-${month}-${day}T00:00:00`); // Ensure time is set to midnight
-        } else {
-          console.error("Invalid date format:", dateString);
-          formattedDate = new Date(); // Return today's date if invalid
-        }
-      }
-    
-      // Check if the parsed date is valid
-      if (isNaN(formattedDate.getTime())) {
-        console.error("Invalid Date object:", dateString);
-        formattedDate = new Date(); // Return today's date if invalid
-      }
-    
-      return formattedDate;
-    };
-    
-    
-    // Format the start and end dates correctly
-    const formattedStartDate = formatDateToObject(startDate);
-    const formattedEndDate = formatDateToObject(endDate);
-  
-    console.log("formattedStartDate", formattedStartDate);
-    console.log("formattedEndDate", formattedEndDate);
-  
-    // Filter data based on date range
-    if (startDate && endDate) {
-      filteredData = filteredData.filter(row => {
-        const invoiceDate = row.invoiceDate; // Get invoiceDate
-  
-        // Convert invoiceDate to Date object
-        const formattedInvoiceDate = formatDateToObject(invoiceDate);
-  
-        console.log("formattedInvoiceDate", formattedInvoiceDate);
-  
-        // Compare dates
-        return formattedInvoiceDate >= formattedStartDate && formattedInvoiceDate <= formattedEndDate;
-      });
-    }
-  
-    setTableData(filteredData);
-  };
-  
+
   
   const getCurrentDate = () => {
     const now = new Date();
@@ -313,7 +218,7 @@ export default function EntityTransactionReportComponent() {
 
 
   const printReport = () => {
-    if (tableData.length === 0) {
+    if (report?.categoryObject.length === 0) {
       return toast.warning('لا يوجد بيانات للطباعة');
 
     }
@@ -348,12 +253,12 @@ export default function EntityTransactionReportComponent() {
     tableData.forEach(row => {
       printWindow.document.write(`
         <tr>
-          <td>${row.code}</td>
-          <td>${row.number}</td>
+          <td>${row.invoiceCode}</td>
+          <td>${row.serialNumber}</td>
           <td>${row.type}</td>
           <td>${row.name}</td>
-          <td>${row.invoiceDate}</td>
-          <td>${row.registrationDate}</td>
+          <td>${row.supplyDate}</td>
+          <td>${row.registerDate}</td>
         </tr>
       `);
     });
@@ -435,7 +340,7 @@ marginBottom:"20px"
       </div>
       <br />
       <div style={{ display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
-        <button style={{ margin: "0 10px 0 0" }} onClick={search} className="btn btn-success">بحث</button>
+        <button style={{ margin: "0 10px 0 0" }} onClick={getReport} className="btn btn-success">بحث</button>
         <button style={{ margin: "0 10px 0 0" }} onClick={cancelSearch} className="btn btn-danger">إلغاء البحث</button>
         <button style={{ margin: "0 10px 0 0" }} onClick={printReport} className="btn btn-primary">طباعة</button>
 
@@ -452,7 +357,7 @@ marginBottom:"20px"
       <div style={{ overflowX: 'auto', marginTop: '20px' }}>
         <DataTable
           columns={columns}
-          data={tableData} // Display the filtered data
+          data={report?.categoryObject} // Display the filtered data
           conditionalRowStyles={[]}
           customStyles={customStyles}
           pagination
